5a3add44bc79d752386c3122050baebe7e316e2e
milliewalky <sovia@disroot.org>
2025-02-25 00:29:56 +0100

    calculate sine using wasm simd intrinsics

--- build.bat
+++ build.bat
@@ -16,7 +16,7 @@ set auto_compile_flags=
 if "%asan%"=="1" set auto_compile_flags=%auto_compile_flags% -fsanitize=address && echo [asan enabled]
 
 :: ~ compile defs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-set compile_common=     --target=wasm32-wasi --sysroot=..\code\third_party\wasi\win32\share\wasi-sysroot -I..\code\ -I..\local\ -gcodeview -fdiagnostics-absolute-paths -Wall -Wno-unknown-warning-option -Wno-missing-braces -Wno-unused-function -Wno-writable-strings -Wno-unused-value -Wno-unused-variable -Wno-unused-local-typedef -Wno-deprecated-register -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-single-bit-bitfield-constant-conversion -Wno-compare-distinct-pointer-types -Wno-initializer-overrides -Wno-incompatible-pointer-types-discards-qualifiers -Xclang -flto-visibility-public-std -ferror-limit=10000
+set compile_common=     --target=wasm32-wasi --sysroot=..\code\third_party\wasi\win32\share\wasi-sysroot -I..\code\ -I..\local\ -gcodeview -fdiagnostics-absolute-paths -Wall -Wno-unknown-warning-option -Wno-missing-braces -Wno-unused-function -Wno-writable-strings -Wno-unused-value -Wno-unused-variable -Wno-unused-local-typedef -Wno-deprecated-register -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-single-bit-bitfield-constant-conversion -Wno-compare-distinct-pointer-types -Wno-initializer-overrides -Wno-incompatible-pointer-types-discards-qualifiers -Xclang -flto-visibility-public-std -ferror-limit=10000 -msimd128 -mrelaxed-simd
 set compile_debug=      call ..\code\third_party\wasi\win32\bin\clang.exe -g -O0 -DBUILD_DEBUG=1 %compile_common% %auto_compile_flags%
 set compile_release=    call ..\code\third_party\wasi\win32\bin\clang.exe -g -O2 -DBUILD_DEBUG=0 %compile_common% %auto_compile_flags%
 set link=               -fuse-ld=..\code\third_party\wasi\win32\bin\lld.exe -Xlinker /MANIFEST:EMBED -Xlinker /pdbaltpath:%%%%_PDB%%%% -Xlinker
@@ -27,8 +27,7 @@ if "%debug%"=="1"       set compile=%compile_debug%
 if "%release%"=="1"     set compile=%compile_release%
 
 :: ~ NOTE(mmcwsk): just export procedures, no entry point ~~~~~~~~~~~~~~~~~~~~~
-set compile=%compile% -nostartfiles
-set compile=%compile% -Wl,--no-entry
+set compile=%compile% -nostartfiles -Wl,--no-entry
 
 :: ~ prepare dirs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if not exist build mkdir build
--- code/index/index_main.c
+++ code/index/index_main.c
@@ -1,7 +1,60 @@
-#include "math.h"
+#define function static
 
-__attribute__((export_name("sine"))) double
-sine(double theta)
+function double
+Factorial(unsigned int v)
 {
- return sin(theta);
+ double result = (double)v;
+ while(v > 1)
+ {
+  v -= 1;
+  result *= v;
+ }
+ return result;
+}
+
+function double
+TaylorSineCoefficient(unsigned int power)
+{
+ double sign = (((power-1)/2) % 2) ? -1.0 : 1.0;
+ double result = (sign/Factorial(power));
+ return result;
+}
+
+#include "wasm_simd128.h"
+
+function double
+TaylorSineHorner(unsigned int power_max, double v)
+{
+ double result = 0;
+
+ // taylor polynomial + horner rule applied; vectorised
+ v128_t accumulator_f64x2 = wasm_f64x2_splat(0.0);
+ {
+  double squared = v*v;
+  for (unsigned int power_inv = 1; power_inv <= power_max; power_inv += 2)
+  {
+   unsigned int power = power_max-(power_inv-1);
+
+   v128_t squared_f64x2 = wasm_f64x2_splat(squared);
+   v128_t coefficient_f64x2 = wasm_f64x2_splat(TaylorSineCoefficient(power));
+
+   accumulator_f64x2 = wasm_f64x2_relaxed_madd(accumulator_f64x2, squared_f64x2, coefficient_f64x2);
+  }
+ }
+
+ // extract data from lane
+ {
+  result = wasm_f64x2_extract_lane(accumulator_f64x2, 0);
+  result *= v;
+ }
+
+ return result;
+}
+
+#define Export(name) __attribute__((export_name(#name))) name
+
+double
+Export(Sine)(double X)
+{
+ return TaylorSineHorner(21, X);
 }
